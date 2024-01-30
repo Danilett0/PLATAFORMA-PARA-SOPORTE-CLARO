a68bcc0c1f7143efa34e5d126fa879fb244f6a0a
Danilett0 <daniilo.97@hotmail.com>
2024-01-29 21:54:11 -0500

    retiro restriccion de password

--- src/Components/SoportCos/Hfc/Components/WhatsApp.jsx
+++ src/Components/SoportCos/Hfc/Components/WhatsApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import Form from "./FormsWhatsapp/Form";
 import ResultadoPlantilla from "./FormsWhatsapp/ResultadoPlantilla";
 import Plantillas from "./FormsWhatsapp/Plantillas";
@@ -6,65 +6,50 @@ import Plantillas from "./FormsWhatsapp/Plantillas";
 function WhatsApp() {
   const [enviForm, setEnvioForm] = useState(false);
   const [Informacion, setInformacion] = useState({});
-  const valAccess = useRef();
   const [Opcion, setOpcion] = useState({ visita: false, masiva: false });
-  const [Acces, setAcces] = useState(false);
-
-  const ValidarAcceso = (e) => {
-    e.target.value === "2143" ? setAcces(true) : setAcces(false);
-  };
 
   return (
     <div className="mainPlantillaHfc Whatsapp">
-      {Acces ? (
-        <>
-          <Plantillas />
+      <Plantillas />
 
-          <div className="InfoAdd">
-            <div className="BtnOpciones">
-              <button
-                className="Button Red"
-                onClick={() => {
-                  setOpcion({ visita: true, masiva: false });
-                  setEnvioForm(false);
-                  setInformacion({});
-                }}
-              >
-                Generar Visita
-              </button>
-              <button
-                className="Button Green"
-                onClick={() => {
-                  setOpcion({ visita: false, masiva: true });
-                  setEnvioForm(false);
-                  setInformacion({});
-                }}
-              >
-                Generar Masiva
-              </button>
-            </div>
+      <div className="InfoAdd">
+        <div className="BtnOpciones">
+          <button
+            className="Button Red"
+            onClick={() => {
+              setOpcion({ visita: true, masiva: false });
+              setEnvioForm(false);
+              setInformacion({});
+            }}
+          >
+            Generar Visita
+          </button>
+          <button
+            className="Button Green"
+            onClick={() => {
+              setOpcion({ visita: false, masiva: true });
+              setEnvioForm(false);
+              setInformacion({});
+            }}
+          >
+            Generar Masiva
+          </button>
+        </div>
 
-            {Opcion && (
-              <>
-                {!enviForm ? (
-                  <Form
-                    setInformacion={setInformacion}
-                    setEnvioForm={setEnvioForm}
-                    accion={`${Opcion.masiva ? "masiva" : "visita"}`}
-                  />
-                ) : (
-                  <ResultadoPlantilla Informacion={Informacion} />
-                )}
-              </>
+        {Opcion && (
+          <>
+            {!enviForm ? (
+              <Form
+                setInformacion={setInformacion}
+                setEnvioForm={setEnvioForm}
+                accion={`${Opcion.masiva ? "masiva" : "visita"}`}
+              />
+            ) : (
+              <ResultadoPlantilla Informacion={Informacion} />
             )}
-          </div>
-        </>
-      ) : (
-        <>
-        <h3>Contraseña Requerida</h3>
-        <input style={{display: "flex", margin: "auto"}} type="password" ref={valAccess} onChange={ValidarAcceso} />
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
